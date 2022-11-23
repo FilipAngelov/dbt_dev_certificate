31b7383c2a9cf841d5cf3011d3108b03f2a953ee
fangelovv@gmail.com <fangelovv@gmail.com>
2022-11-23 12:49:11 +0000

    jinja

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -4,7 +4,7 @@ with payments as (
     orderid as order_id,
     paymentmethod as payment_method,
     status,
-    amount/100 as amount, 
+    amount/100 as amount,
     created as created_at
 from
     {{ source('stripe', 'payment') }}
